3b241f504e174e0fc6af6c037faefffb9ebbaac3
AndresPautrat <38121556+AndresPautrat@users.noreply.github.com>
2018-09-09 14:36:23 -0500

    Update Programa.py

--- Programa.py
+++ Programa.py
@@ -1,4 +1,2 @@
-Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:25:58) [MSC v.1500 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
 >>> print ("hello world")
     print ("hello world")
